958327ec93030e156d6b14fa9017bd471fb09489
Lusjunior05 <71136421+Lusjunior05@users.noreply.github.com>
2022-08-26 15:40:22 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//teste 01 versão 1.1
+//teste 01 versão 1.1 altera
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
